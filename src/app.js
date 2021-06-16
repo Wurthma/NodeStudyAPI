28ec60cbf76d1c80445f91b9d855880ebe388b4a
George Henrique Wurthmann <gemakai@gmail.com>
2021-06-15 22:13:32 -0300

    Habilitando CORS

--- src/app.js
+++ src/app.js
@@ -18,8 +18,15 @@ const ordersRoute = require('./routes/order-route');
 // Anteriormente era comum o uso do body-parser para converter o conteúdo do body para JSON, agora o express já faz esse trabalho
 // Mais informações: https://stackoverflow.com/a/59892173/5522115
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '5mb'}));
 
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 // Conectar ao MongoDB
 mongoose.connect('mongodb://wurthmann:5777308@localhost:27017/ndstr?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
